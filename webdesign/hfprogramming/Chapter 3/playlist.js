a5cfecdef82c5fe4576bdb46da71f7b098604533
Kaya DeMarco <kaya2005demarco@gmail.com>
2022-02-11 14:24:31 -0500

    Add playlist javascript

--- webdesign/hfprogramming/Chapter 3/playlist.js
+++ webdesign/hfprogramming/Chapter 3/playlist.js
@@ -1,15 +1,28 @@
 //Creating Add Song Button below. The following allows us to make the button add a song to the list
-var button = document.getElementById("addbutton") ;
-
-button.onclick = handleButtonClick;
-
 window.onload = init;
-function init()
-    {
-        var button = document.getElementById ("addButton");
-        button.onclick = handleButtonClick;
-    }
-function handleButtonClick()
-    {
-        alert ("Button was clicked!")
-    }
+
+function init() {
+	var button = document.getElementById("addButton");
+	button.onclick = handleButtonClick;
+
+	loadPlaylist();
+}
+
+function handleButtonClick(e) {
+	var textInput = document.getElementById("songTextInput");
+	var songName = textInput.value;
+
+	if (songName == "") {
+		alert("Please enter a song");
+	}
+	else {
+		var li = document.createElement("li");
+		li.innerHTML = songName;
+		var ul = document.getElementById("playlist");
+		ul.appendChild(li);
+
+		save(songName);
+	}
+}
+
+
